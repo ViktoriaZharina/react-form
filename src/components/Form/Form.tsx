3acec69090c7e5bab71a14961634859f7ffb72f8
Viktoria Zharina <159477668+ViktoriaZharina@users.noreply.github.com>
2025-05-26 12:27:11 +0300

    add form

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,27 @@
+import "./Form.module.css";
+
+export default function Form() {
+  return (
+    <form className="form">
+      <div className="form-group">
+        <label htmlFor="name">Ім’я:</label>
+        <input type="text" id="name" name="name" className="form-input" />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="email">Імейл:</label>
+        <input type="email" id="email" name="email" className="form-input" />
+      </div>
+
+      <div className="form-group checkbox-group">
+        <input
+          type="checkbox"
+          id="delivery"
+          name="delivery"
+          className="form-checkbox"
+        />
+        <label htmlFor="delivery">Хочу доставку</label>
+      </div>
+    </form>
+  );
+}
